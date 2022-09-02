72a887cd3d9a0f423a0baf8fa3362b9dc225f92e
Ibrahim S <ibsofficials@gmail.com>
2022-09-02 03:33:50 +0100

    basic functionality for calculator

--- app.js
+++ app.js
@@ -1,54 +1,82 @@
 const resultScreen = document.querySelector(".resultScreen");
 const bottomScreen = document.querySelector(".bottomScreen");
 const allNums = document.querySelectorAll(".nums");
+const clear = document.querySelector(".clear");
+const operators = document.querySelectorAll(".oprator");
+const backspace = document.querySelector(".backspace");
+const btns = document.querySelectorAll(".btn");
+const equals = document.querySelector(".equals");
+
+let chosenOperator = null;
+let firstOperand = "";
+let secondOperand = "";
 
 
-// const add = function (...nums) {
-//     return total = nums.reduce((num1, num2) => num1 + num2)
-// }
-// const subtract = function (...nums) {
-//     return total = nums.reduce((num1, num2) => num1 - num2)
-// }
-// const multiply = function (...nums) {
-//     return total = nums.reduce((num1, num2) => num1 * num2)
-// }
-// const divide = function (...nums) {
-//     return total = nums.reduce((num1, num2) => num1 / num2)
-// }
 
 const add = function (x, y) {
     return x + y
-}
+};
 const subtract = function (x, y) {
     return x - y
-}
+};
 const multiply = function (x, y) {
     return x * y
-}
+};
 const divide = function (x, y) {
     return x / y
-}
+};
 
-const operators = function (operator, num1, num2) {
+const operate = function (operator, num1, num2) {
     num1 = Number(num1)
     num2 = Number(num2)
     switch (operator) {
-        case "add":
-            add(num1, num2);
-            break;
-        case "subtract":
-            subtract(num1, num2);
-            break;
-        case "multiply":
-            multiply(num1, num2);
-            break;
-        case "divide":
-            divide(num1, num2);
-            break
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "×":
+            return multiply(num1, num2);
+        case "%":
+            return divide(num1, num2);
     }
 }
+function reset() {
+    chosenOperator = null;
+    firstOperand = ""
+    secondOperand = ""
+    bottomScreen.innerText = ""
+    resultScreen.innerText = ""
+}
 
-allNums.forEach(num => num.addEventListener("click", evt => {
-    evt.target.value = evt.target.innerText
-    console.log(evt.target.value)
+clear.addEventListener("click", reset);
+
+backspace.addEventListener("click", () => {
+    let str = bottomScreen.innerText;
+    let newStr = str.slice(0, -1)
+    bottomScreen.innerText = ''
+    bottomScreen.append(newStr)
+
+})
+allNums.forEach(num => num.addEventListener("click", (evt) => {
+    const btnValue = evt.target.value = evt.target.innerText;
+    bottomScreen.append(btnValue);
 }))
+
+operators.forEach(operator => operator.addEventListener("click", (evt) => {
+    const btnValue = evt.target.value = evt.target.innerText;
+    const bottomScreenValue = bottomScreen.innerText;
+    chosenOperator = btnValue;
+    resultScreen.append(`${bottomScreenValue} ${chosenOperator}`)
+    firstOperand = bottomScreenValue
+    bottomScreen.innerText = "";
+}))
+
+equals.addEventListener("click", () => {
+    secondOperand = bottomScreen.innerText;
+    resultScreen.innerText = ""
+    resultScreen.append(`${firstOperand} ${chosenOperator} ${secondOperand}`);
+    const result = operate(chosenOperator, firstOperand, secondOperand)
+    bottomScreen.innerText = ""
+    bottomScreen.append(result)
+    console.log(result)
+})
